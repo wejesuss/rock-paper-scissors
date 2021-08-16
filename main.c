451796d02f5a0d6a8e86b8e61b26badfa7a3db79
Wemerson Jesus <wejesuss@gmail.com>
2021-08-16 10:07:34 -0300

    fix: grammar errors
    - fix grammar errors from source code

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ typedef struct Pick
     uint8_t code;
     // 0 - rock
     // 1 - paper
-    // 2 scissors
+    // 2 - scissors
 } Pick;
 
 typedef struct Player
@@ -24,7 +24,7 @@ int get_valid_integer()
 
     if (result == EOF)
     {
-        printf("Nothing was read\n");
+        printf("Nothing has been read\n");
         return -1;
     }
 
@@ -65,18 +65,18 @@ uint8_t random_pick(uint8_t lower, uint8_t upper)
 
 void show_winner(Player player1, Player player2, uint8_t upper)
 {
-    uint8_t loss = (player1.pick.code + 1) % (upper + 1);
-    uint8_t win = (player1.pick.code + 2) % (upper + 1);
+    uint8_t loses = (player1.pick.code + 1) % (upper + 1);
+    uint8_t wins = (player1.pick.code + 2) % (upper + 1);
     uint8_t code = player2.pick.code;
 
     printf("\n");
-    if (code == loss)
+    if (code == loses)
     {
-        printf("Player 2 wins!\n");
+        printf("Player 2 won!\n");
     }
-    else if (code == win)
+    else if (code == wins)
     {
-        printf("Player 1 wins!\n");
+        printf("Player 1 won!\n");
     }
     else
     {
@@ -87,7 +87,7 @@ void show_winner(Player player1, Player player2, uint8_t upper)
 int main(void)
 {
     uint8_t lower = 0, upper = 2;
-    printf("Enter some integer between 0 and 2\n\n");
+    printf("Enter any integer between 0 and 2\n\n");
     printf("0 = Rock\n1 = Paper\n2 = Scissors\n\n");
 
     int value = get_valid_integer();
